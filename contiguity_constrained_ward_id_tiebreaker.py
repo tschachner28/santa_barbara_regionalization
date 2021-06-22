4be66bfdced495372222925c677cec6f7958e5d6
tschachner28 <53120075+tschachner28@users.noreply.github.com>
2021-06-21 20:50:44 -0400

    added output file for smallest combined id method

--- contiguity_constrained_ward_id_tiebreaker.py
+++ contiguity_constrained_ward_id_tiebreaker.py
@@ -1,10 +1,39 @@
 import networkx as nx
 import pandas as pd
 
-# Helper function to calculate SSD
+# Helper function to calculate within-region SSD (for one region)
+# Input: r (list of IDs in the region)
+# Output: SSD of r
+def get_SSD_one_region(r):
+    ssd = 0
+
+    r_pol_sum = sum(political_data_dict[point] for point in r) if len(r) > 1 else political_data_dict[r[0]]
+    r_pol_mean = r_pol_sum / len(r)
+    r_ext_sum = sum(ext_data_dict[point] for point in r) if len(r) > 1 else ext_data_dict[r[0]]
+    r_ext_mean = r_ext_sum / len(r)
+    r_agr_sum = sum(agr_data_dict[point] for point in r) if len(r) > 1 else agr_data_dict[r[0]]
+    r_agr_mean = r_agr_sum / len(r)
+    r_con_sum = sum(con_data_dict[point] for point in r) if len(r) > 1 else con_data_dict[r[0]]
+    r_con_mean = r_con_sum / len(r)
+    r_neu_sum = sum(neu_data_dict[point] for point in r) if len(r) > 1 else neu_data_dict[r[0]]
+    r_neu_mean = r_neu_sum / len(r)
+    r_ope_sum = sum(ope_data_dict[point] for point in r) if len(r) > 1 else ope_data_dict[r[0]]
+    r_ope_mean = r_ope_sum / len(r)
+
+    for point in r:
+        ssd += pow(political_data_dict[point] - r_pol_mean, 2)
+        ssd += pow(ext_data_dict[point] - r_ext_mean, 2)
+        ssd += pow(agr_data_dict[point] - r_agr_mean, 2)
+        ssd += pow(con_data_dict[point] - r_con_mean, 2)
+        ssd += pow(neu_data_dict[point] - r_neu_mean, 2)
+        ssd += pow(ope_data_dict[point] - r_ope_mean, 2)
+
+    return ssd
+
+# Helper function to calculate across-region SSD (for two regions)
 # Inputs: r1 (list of IDs in first region), r2 (list of IDs in second region)
-# Outputs: float (the SSD)
-def get_SSD(r1, r2):
+# Outputs: SSD of r1 and r2, SSD of r1, SSD of r2 (all floats)
+def get_SSD_two_regions(r1, r2):
     regions = [r1, r2]
     regions_str = ['r1', 'r2']
     ssd_list = [0, 0] # [r1_ssd, r2_ssd]
@@ -73,12 +102,22 @@ def get_SSD(r1, r2):
         ssd_r1_r2 += pow(neu_data_dict[point] - r1_r2_neu_mean, 2)
         ssd_r1_r2 += pow(ope_data_dict[point] - r1_r2_ope_mean, 2)
 
-    return ssd_r1_r2 - ssd_list[0] - ssd_list[1]
+    return ssd_r1_r2 - ssd_list[0] - ssd_list[1], ssd_list[0], ssd_list[1]
 
 
 # Load data from .csv files
-variables = pd.read_csv('all6variables_regionalization_w_pop_density.xlsx - ALL6VARIABLES.csv', delimiter=',')
-contiguity_data = pd.read_csv('political_regionalization - CONTIGUITY.csv', delimiter=',')
+variables = pd.read_csv('all6variables_regionalization_final.xlsx - ALL6VARIABLES.csv', delimiter=',')
+contiguity_data = pd.read_csv('all6variables_regionalization_final.xlsx - CONTIGUITY.csv', delimiter=',')
+
+# Output file
+data_filename = 'regionalization_id_tiebreaker.txt'
+headers = 'r_u, r_v, r_u SSD, r_v SSD, r_u and r_v SSD\n'  # Column names
+file1 = open(data_filename, "w")
+file1.writelines(headers)
+
+#widths = [5,5,5,5,5]
+#format_spec = '{:{widths[0]}}  {:>{widths[1]}}  {:>{widths[2]}}  {:>{widths[3]}}  {:>{widths[4]}}'
+#print(format_spec.format(*headers, widths=widths))
 
 # Step 1
 R = pd.DataFrame(variables, columns= ['ID#']).values.tolist() # regions (set of 500 data points)
@@ -92,37 +131,37 @@ for p in political_data_list:
 
 # EXT data
 ext_data_dict = {}
-ext_data_list = pd.DataFrame(variables, columns= ['ID#', 'EXT']).values.tolist()
+ext_data_list = pd.DataFrame(variables, columns= ['ID#', 'AVG_EXTROVERT']).values.tolist()
 for e in ext_data_list:
     ext_data_dict[e[0]] = e[1]
 
 # AGR data
 agr_data_dict = {}
-agr_data_list = pd.DataFrame(variables, columns= ['ID#', 'AGR']).values.tolist()
+agr_data_list = pd.DataFrame(variables, columns= ['ID#', 'AVG_AGREEABLE']).values.tolist()
 for a in agr_data_list:
     agr_data_dict[a[0]] = a[1]
 
 # CON data
 con_data_dict = {}
-con_data_list = pd.DataFrame(variables, columns= ['ID#', 'CON']).values.tolist()
+con_data_list = pd.DataFrame(variables, columns= ['ID#', 'AVG_CONSCIENTIOUS']).values.tolist()
 for c in con_data_list:
     con_data_dict[c[0]] = c[1]
 
 # NEU data
 neu_data_dict = {}
-neu_data_list = pd.DataFrame(variables, columns= ['ID#', 'NEU']).values.tolist()
+neu_data_list = pd.DataFrame(variables, columns= ['ID#', 'AVG_NEUROTIC']).values.tolist()
 for n in neu_data_list:
     neu_data_dict[n[0]] = n[1]
 
 # OPE
 ope_data_dict = {}
-ope_data_list = pd.DataFrame(variables, columns= ['ID#', 'OPE']).values.tolist()
+ope_data_list = pd.DataFrame(variables, columns= ['ID#', 'AVG_OPEN']).values.tolist()
 for o in ope_data_list:
     ope_data_dict[o[0]] = o[1]
 
 # Step 2: Initialize graph's edge set to empty set
 G = nx.Graph()
-C_list = pd.DataFrame(contiguity_data, columns= ['source_ID', 'neighbor_ID']).values.tolist()
+C_list = pd.DataFrame(contiguity_data, columns= ['source_ID', 'nbr_ID']).values.tolist()
 C = {} # contiguity dictionary
 for c in C_list:
     if c[0] not in C.keys():
@@ -135,7 +174,7 @@ for c in C_list:
 for r_u in R:
     for r_v in R:
         if (r_u[0] in C.keys() and r_v[0] in C[r_u[0]]) or (r_v[0] in C.keys() and r_u[0] in C[r_v[0]]):
-            edge_weight = get_SSD(r_u, r_v)
+            edge_weight, r_u_ssd, r_v_ssd = get_SSD_two_regions(r_u, r_v)
             G.add_edge(tuple(r_u), tuple(r_v), weight=edge_weight) # need to convert r_u and r_v from lists to tuples first
 
 # Step 4: iteratively remove the minimum length edge to merge IDs into two major regions
@@ -149,9 +188,10 @@ while len(R) > 2 and len(list(G.edges())) > 1:
     r_u = ()
     r_v = ()
 
+
+
     # if multiple shortest edges, choose the one connecting the regions with the smallest combined ID
     if len(e_star_r1_list) > 1:
-
         min_combined_id = float('inf')
         r_u = None
         r_v = None
@@ -166,6 +206,10 @@ while len(R) > 2 and len(list(G.edges())) > 1:
         r_u = e_star_r1_list[0]
         r_v = e_star_r2_list[0]
 
+    r_u_r_v_ssd = e_star
+    r_u_ssd = get_SSD_one_region(r_u)
+    r_v_ssd = get_SSD_one_region(r_v)
+
 
     if while_loop_repeats == 5:
         y = 1
@@ -177,16 +221,17 @@ while len(R) > 2 and len(list(G.edges())) > 1:
     R.append(list(new_r)) # add r_u = r_u U r_v (union of both) to R
     edges_to_append = [] # list of lists, each of which contains [v1, v2, weight] representing the new edge
 
+
     # redirect edges incident on r_u to new_r, and edges incident on r_v to new_r, with the new weights
     for e in list(G.edges()):
         if e[0] == r_u or e[0] == r_v:
-            new_weight = get_SSD(new_r, e[1])
+            new_weight, ssd_ru, ssd_rv = get_SSD_two_regions(new_r, e[1])
             G.remove_edge(e[0], e[1])
             edges_to_append.append([new_r, e[1], new_weight])
         if e[1] == r_u or e[1] == r_v:
             if while_loop_repeats == 432:
                 z = 2
-            new_weight = get_SSD(e[0], new_r)
+            new_weight, ssd_ru, ssd_rv = get_SSD_two_regions(e[0], new_r)
             G.remove_edge(e[0], e[1])
             edges_to_append.append([e[0], new_r, new_weight])
 
@@ -196,9 +241,21 @@ while len(R) > 2 and len(list(G.edges())) > 1:
             z = 2
         G.add_edge(edge[0], edge[1], weight=edge[2])
 
+    output_data = r_u, r_v, r_u_ssd, r_v_ssd, r_u_r_v_ssd
+    #print(format_spec.format(*output_data, widths=widths))
+    file1.write(str(output_data[0]) + ', ' + str(output_data[1]) + ', ' + str(output_data[2]) + ', ' + str(output_data[3]) + ', ' + str(output_data[4]) + '\n')
+
     while_loop_repeats += 1
 
+
+
+
 x=0
-print(R)
+print("Final R: " + str(R))
+for i, r in enumerate(R):
+    print("IDs in R" + str(i) + ": " + str(r))
+    print("R" + str(i) + " Length: " + str(len(r)))
+
+
 
 
